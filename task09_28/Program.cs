4aebdb286b8c0cf9210eca3cf0c84ee88b671208
ZulfiyyaQ <tu7u71zbq@code.edu.az>
2023-09-28 20:18:00 +0400

    task09_28

---
+++ task09_28/Program.cs
@@ -0,0 +1,52 @@
+﻿//task1
+
+//int[] arr = { 1, 5, 7, 3, 9, 6 };
+//int sum = 0;
+//for (int i=0;i<arr.Length;i++)
+//{
+//    sum += arr[i];
+
+//}
+//Console.WriteLine(sum);
+
+
+
+
+//task2
+
+
+//string[] names = { "Zuzu", "Mehriban", "Sebine", "Emil", "Sofia" };
+//for (int i = 0; i < names.Length; i++)
+//{
+//    if (names[i].Length>4)
+//    {
+//        Console.WriteLine(names[i]);
+//    }
+//}
+
+
+
+
+//task3
+
+
+//int[] arr = { 1, 5, 7, 3, 9, 6 ,11,-3,2};
+//int temp = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+
+//    for (int j = i + 1; j < arr.Length; j++)
+//    {
+//        if (arr[i] < arr[j])
+//        {
+//            temp = arr[i];
+//            arr[i] = arr[j];
+//            arr[j] = temp;
+            
+//        }
+//    }
+//    Console.WriteLine(arr[i]);
+//}
+
+
+
